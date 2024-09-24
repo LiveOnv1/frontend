d97c58b72cce24afcc10b97954bf3f7e544da1ff
lee <skystar9927@naver.com>
2024-09-24 18:02:43 +0900

    #11 chore: 프론트 초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  background: black;
+  color: white;
+`;
+
+function App() {
+  return (
+    <Div>hello</Div>
+  );
+}
+
+export default App;
