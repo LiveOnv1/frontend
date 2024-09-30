80ef4af75a21b0869f4ab4d5569ef46f443399d0
lee <skystar9927@naver.com>
2024-09-30 18:02:37 +0900

    #23 feat: 회원가입 페이지 레이아웃 구현

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,14 +1,48 @@
+// src/pages/Login.tsx
 import styled from 'styled-components';
+import AuthForm from '../components/common/AuthForm';
+import AuthLayout from '../components/common/AuthLayout';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  const URL = 'http://3.39.74.178:8080';
+
+  const handleSignup = async (id: string, password: string) => {
+    try {
+      const response = await axios.post(URL + '/api/register', {
+        personId: id,
+        personPassword: password,
+      });
+
+      console.log("Regi successful:", response.data);
+      localStorage.setItem('token', response.data.token);
+      window.location.href = "/";
+      
+    } catch (error) {
+      console.error("Regi failed", error);
+      alert('Regi failed. Please try again.');
+    }
+  };
+
   return (
-    <Div>Signup</Div>
+    <AuthLayout title="회원가입">
+      <AuthForm mode="signup" onAuthSubmit={handleSignup}>
+        <GotoLogin to="/login">
+          로그인 하러가기
+        </GotoLogin>
+      </AuthForm>
+    </AuthLayout>
   );
 }
 
-const Div = styled.div`
-  background: black;
-  color: white;
+const GotoLogin = styled(Link)`
+  color: #3081F6;
+  margin-top: 34px;
+  text-decoration: underline;
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default Signup;
