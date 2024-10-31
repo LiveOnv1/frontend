2a916335c6d7d3afb84d95f811d674b3e0e2c51d
lee <skystar9927@naver.com>
2024-10-31 15:01:41 +0900

    #19 feat: 채팅섹션UI구현

---
+++ src/api/chatApi.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000/'; // json-server 주소
+
+export const getChats = async (idx: number) => {
+  const response = await axios.get(BASE_URL+idx);
+  return response.data;
+};
+
+export const postChat = async (chatData: { content: string; sender: string; timestamp: string }) => {
+  const response = await axios.post(BASE_URL, chatData);
+  return response.data;
+};
--- src/components/home/ChatMainContents.tsx
+++ src/components/home/ChatMainContents.tsx
@@ -1,68 +1,111 @@
 import { useStore } from '../../store/store';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { getChats } from '../../api/chatApi';
+import ProfileIcon from '../../assets/ProfileIcon.png';
+
+interface Chat {
+  content: string;
+  sender: string;
+  timestamp: string;
+}
+interface Channel {
+  channelName: string;
+  chats: Chat[];
+}
+
+interface Category {
+  category: string;
+  channels: Channel[];
+}
 
 const ChatMainContents = () => {
   const selectedCategory = useStore((state) => state.selectedCategory);
   const selectedChannel = useStore((state) => state.selectedChannel);
+  const [chats, setChats] = useState<Chat[]>([]);
+
+  useEffect(() => {
+    const fetchChats = async () => {
+      const idx = selectedCategory == '백엔드' ? 0 : ('프론트엔드' ? 1 : -1)
+      const data: Category = await getChats(idx); // getChats의 반환값 타입 지정
+      const filteredChats = data.channels
+        .find((ch) => ch.channelName === selectedChannel)?.chats || [];
+      setChats(filteredChats);
+    };
+    fetchChats();
+  }, [selectedCategory, selectedChannel]);
 
   return (
     <Container>
-      {!selectedChannel ? 
-        <div>채널을 선택해주세요.</div> 
-      : 
         <Content>
-          {'선택된 카테고리 : ' + selectedCategory}
-          <br />
-          {'선택된 채널 : ' + selectedChannel}
-          <br />
-          {selectedCategory + ' 카테고리의 ' + selectedChannel + ' 채널 채팅 : '}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nobis. Commodi laborum dolore tempora. Quia fuga, quidem, quo quaerat, praesentium sapiente aliquid id commodi modi deserunt harum amet sit perspiciatis!
+          {chats.map((chat, index) => (
+            <ChatWrapper key={index}>
+              <ChatProfileImg src={ProfileIcon}/>
+              <ChatContentWrapper>
+                <ChatSender>{chat.sender}</ChatSender>
+                <ChatCTWraper>
+                  <ChatContent>{chat.content}</ChatContent>
+                  <ChatTime>
+                    {new Date(chat.timestamp).toLocaleTimeString("ko-KR", { 
+                      hour: "numeric",
+                      minute: "numeric",
+                      hour12: true,})}
+                  </ChatTime>
+                </ChatCTWraper>
+              </ChatContentWrapper>
+            </ChatWrapper>
+          ))}
         </Content>
-      }
     </Container>
   );
 }
 
 const Container = styled.div`
+  width: 100%;
   height: calc(100vh - 76px - 98px);
   overflow: auto;
   margin-top: 76px;
+  display: flex;
+  justify-content: center;
 `;
 const Content = styled.div`
+  width: 90%;
 `;
+const ChatWrapper = styled.div`
+  margin-top: 24px;
+  display: flex;
+`;
+const ChatProfileImg = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+`;
+const ChatContentWrapper = styled.div`
+  margin-left: 12px;
+`;
+const ChatSender = styled.div`
+  height: 24px;  
+  font-size: 18px;
+  margin-bottom: 6px;
+`;
+const ChatContent = styled.div`
+  background-color: #D9D9D9;
+  padding: 16px;
+  border-radius: 20px;
+  max-width: 660px;
+  font-size: 16px;
+`;
+const ChatTime = styled.div`
+  color: #777777;
+  font-size: 14px;
+  margin-left: 2px;
+  display: flex;
+  align-items: end;
+`;
+const ChatCTWraper = styled.div`
+  display: flex;
+`;
+
+
 
 export default ChatMainContents;
